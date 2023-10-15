76762c634e65cb3294a9258a280b9f49f473903b
Albanderi <Albanderi-bh@hotmail.com>
2023-10-15 10:13:49 +0300

    aa

--- PopUpCode.swift
+++ PopUpCode.swift
@@ -4,15 +4,95 @@
 //
 //  Created by Albanderi Binhasher on 30/03/1445 AH.
 //
-
 import SwiftUI
 
-struct PopUpCode: View {
+struct CustomDialog: View {
+    @Binding var isActive: Bool
+
+    let title: String
+    let message: String
+    let buttonTitle: String
+    let action: () -> ()
+    @State private var offset: CGFloat = 1000
+    @State var textFieldText: String = ""
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color(.black)
+                .opacity(0.5)
+                .onTapGesture {
+                    close()
+                }
+
+            VStack {
+                Text(title)
+                    .font(.title2)
+                    .bold()
+                    .padding()
+                    .foregroundColor(Color("C4"))
+
+                Text(message)
+                    .font(.body)
+                
+                TextField("Write Code ",text: $textFieldText)
+                    .padding(.leading, 14.0)
+                    .frame(width: 200, height: 40)
+                    .background(Color("C3"))
+                    .accessibilityLabel(/*@START_MENU_TOKEN@*/"Label"/*@END_MENU_TOKEN@*/)
+
+                Button {
+                    action()
+                    close()
+                } label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(Color("C1")) // Change the color to your desired color
+
+                        Text(buttonTitle)
+                            .font(.system(size: 16, weight: .bold))
+                            .foregroundColor(.white)
+                            .padding()
+                    }
+                    .padding()
+                }
+            }
+            .fixedSize(horizontal: false, vertical: true)
+            .padding()
+            .background(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 20))
+            .overlay(alignment: .topTrailing) {
+                Button {
+                    close()
+                } label: {
+                    Image(systemName: "xmark")
+                        .font(.title2)
+                        .fontWeight(.medium)
+                }
+                .tint(.black)
+                .padding()
+            }
+            .shadow(radius: 20)
+            .padding(30)
+            .offset(x: 0, y: offset)
+            .onAppear {
+                withAnimation(.spring()) {
+                    offset = 0
+                }
+            }
+        }
+        .ignoresSafeArea()
+    }
+
+    func close() {
+        withAnimation(.spring()) {
+            offset = 1000
+            isActive = false
+        }
     }
 }
 
-#Preview {
-    PopUpCode()
+struct CustomDialog_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomDialog(isActive: .constant(true), title: "Enter Code", message: "", buttonTitle: "Join", action: {})
+    }
 }
