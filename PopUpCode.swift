5805f287a31253a9cb6cebf6adad94b878d3e450
Albanderi <Albanderi-bh@hotmail.com>
2023-10-15 10:10:58 +0300

    lll

---
+++ PopUpCode.swift
@@ -0,0 +1,18 @@
+//
+//  PopUpCode.swift
+//  saduAnimetion
+//
+//  Created by Albanderi Binhasher on 30/03/1445 AH.
+//
+
+import SwiftUI
+
+struct PopUpCode: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PopUpCode()
+}
